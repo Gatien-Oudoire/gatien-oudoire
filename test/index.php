9b6255b3483cbb73da49d3bfb9fae2ba010a87fe
Naguite <37880199+Naguite@users.noreply.github.com>
2020-05-20 10:31:37 +0200

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,7 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+$reponse = $bdd->query('SELECT * FROM jeux_video');
+while ($donnees = $reponse->fetch()){
+    echo '<p>', $donnees['nom'], '</p>';
+}
+?>
